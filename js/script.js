a2145a6059848f2b87918098381bb65b014cca7f
tobbe <devtobb@gmail.com>
2019-03-20 00:20:58 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,130 @@
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+
+Date.prototype.toTimeInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(11,19);
+});
+
+function formatDate(date){
+    var formattedDate = new Date(date);
+    var d = formattedDate.getDate();
+    var m =  formattedDate.getMonth() + 1;
+    var y = formattedDate.getFullYear();
+    
+    return d + "." + m + "." + y
+}
+
+function populateDropdowns(){
+    casetypes = ["1A", "1S", "DA", "FL", "PP"];
+    poes = ["MZEN-EH", "WZEN-EHC", "WZEN-EHI", "WZEN-EHK", "SZEN-EH"]
+    foes = ["MAUFN", "MORTHO", "MHNO", "MGYN", "MDERMA", "MNEURO", "MALLG-CH", "MAN-I", "MIN-KARD", "MSTR", "MGEB-H", "MIN-NEPH", "MIN-GAST", "MIN-HÄON", "MIN-INF", "WAUFN-C", "WAUFN", "WUNF-CH", "WHNO", "WMKG", "WAUGEN", "WNEURO", "WNEU-CH", "WALLG-CH", "WGYN", "WAUFN", "WAN-I", "WIN-KARD", "WSTR", "WACH-I", "WPNKJ", "WKI-H/O", "WKI-PAED", "WKI-CH", "WGEB-H", "WKI-NEO", "WAUF-N", "WIN-NEPH", "WNP-I", "WIN-KART", "WIN-GAST", "WHO-I", "WIN-HÄON", "WIN-INF", "SAUFN-C", "SUNF-CH", "SHNO", "SMKG", "SAUGEN", "SNEURO", "SNEU-CH", "SALLG-CH", "SGYN", "SAUFN", "SAN-I", "SIN-KARD", "SSTR", "SACH-I", "SPNKJ", "SKI-H/O", "SKI-PAED", "SKI-CH", "SGEB-H", "SKI-NEO", "SAUF-N", "SIN-NEPH", "SNP-I", "SIN-KART", "SIN-GAST", "SHO-I", "SIN-HÄON", "SIN-INF"];
+    sexes = ['männlich', 'weiblich', 'unbekannt']
+
+    // add casetypes
+    $.each(casetypes, function(key, value) {   
+        $('#casetype')
+            .append($("<option></option>")
+                       .attr("value",value)
+                       .text(value)); 
+   });
+
+    // add poes
+    $.each(poes, function(key, value) {   
+        $('#poe')
+            .append($("<option></option>")
+                        .attr("value",value)
+                        .text(value)); 
+    });
+    
+    // add foes
+    $.each(foes, function(key, value) {   
+        $('#foe')
+            .append($("<option></option>")
+                        .attr("value",value)
+                        .text(value)); 
+    });
+
+    // add sexes
+    $.each(sexes, function(key, value) {   
+        $('#sex')
+            .append($("<option></option>")
+                        .attr("value",value)
+                        .text(value)); 
+    });
+}
+
+function updateStickers(){
+    // do the ones where the fields map one on one
+    fields = ['caseno', 'casetype', 'poe', 'foe', 'name', 'firstname', 'casetime', 'street', 'zip', 'city', 'telephone', 'insurance', 'insurancenum', 'insurancetype']
+    $.each(fields, function(key, value){
+        $('[name="'+value+'"]').html($('#'+value).val())
+    });
+
+    // do corner cases by hand
+    
+    $('[name="birthday"]').html(formatDate($('#birthday').val()))
+    $('[name="casedate"]').html(formatDate($('#casedate').val()))
+
+    var sex;
+    switch($('#sex').val()){
+        case 'männlich':
+            sex = 'M'
+            break;
+        case 'weiblich':
+            sex = 'W'
+            break;
+        case 'unbekannt':
+            sex = 'U'
+            break;
+    }
+    $('td[name="sex"]').html(sex)
+    // $('td[name="name"]').html($('#name').val())
+    // $('td[name="name"]').html($('#name').val())
+    // $('td[name="name"]').html($('#name').val())
+}
+
+
+function initForm() {
+    // duplicate sticker templates dom
+    var $sticker = $("#stickertemplate").clone();
+    $('.stickercontainer').html($sticker);
+    
+    // add options to select dropdowns
+    populateDropdowns()
+
+    // register cb if form content changed
+    $(".nofakle-form").change(updateStickers)
+
+    // register cb for print button
+    $("#printbutton").click(function(){
+        window.print()
+    });
+
+    // register cb for reset button
+    $("#resetbutton").click(function(){
+        if(confirm("Sicher? Alle nicht gedruckten Daten gehen verloren!")){
+            clearForm();
+        }
+    });
+
+    // clear form
+    clearForm()
+}
+
+function clearForm(){
+
+    // erase fields content
+    $("#mainform").trigger("reset");
+
+    // set current date and time
+    $("#casedate").val(new Date().toDateInputValue());
+    $("#casetime").val(new Date().toTimeInputValue());
+    
+}
+
+$(document).ready(initForm);
